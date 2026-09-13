85d600d951998b20b2d7a60de1ee3c84bfb30c36
Umang Jaiswal <umangraj84008@gmail.com>
2026-09-13 14:03:49 +0530

    Time: 203 ms (80.00%) | Memory: 15.4 MB (40.00%) - LeetSync

--- 4406-minimum-days-to-score-exactly-n-points/minimum-days-to-score-exactly-n-points.cpp
+++ 4406-minimum-days-to-score-exactly-n-points/minimum-days-to-score-exactly-n-points.cpp
@@ -1,23 +1,13 @@
 class Solution {
 public:
     int minDays(int n) {
-        const int INF = INT_MAX / 2;
-        vector<int> dp(n + 1, INF);
+        vector<int> dp(n + 1, INT_MAX);
         dp[0] = 0;
-
-        int maxK = 1;
-        while((long long)(maxK+1) * (maxK+2) / 2 <= n) maxK++;
-
-        for(int s = 1; s <= n; s++){
-            for(int k = 1; k <= maxK; k++){
-                long long t = (long long)k * (k + 1) / 2;
-                if(t > s) break;
-                if(dp[s - t] != INF){
-                    dp[s] = min(dp[s], dp[s - (int)t] + k + 1);
-                }
+        for (int i = 1; i <= n; ++i) {
+            for (int d = 1, j = 1; j <= i; j += ++d) {
+                dp[i] = min(dp[i], dp[i - j] + d + (i != j));
             }
         }
-
-        return dp[n] - 1;
+        return dp[n];
     }
 };
